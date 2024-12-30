99c6fbb9b367c24d096519b4de2ab85e4317a096
Siphosakhe2583003 <siphosakhemkhwanazi.github@gmail.com>
2024-12-30 02:13:53 +0200

    Convert to srt works

--- source/main.py
+++ source/main.py
@@ -2,16 +2,15 @@ import os
 
 import cv2 as cv
 import easyocr
-from deep_translator import GoogleTranslator
-
+from deep_translator import DeeplTranslator
 from utils import are_images_similar, format_timestamp
 
 reader = easyocr.Reader(['ja'])
-cap = cv.VideoCapture('../videos/EP.1.v0.1639462663.720p.mp4')
+cap = cv.VideoCapture('../videos/test.mp4')
 fps = cap.get(cv.CAP_PROP_FPS)
 timestamps = []
 data_from_frames = {}
-frame_number = 0
+frame_number = 1
 prev_frame = None
 font = cv.FONT_HERSHEY_PLAIN
 
@@ -25,39 +24,42 @@ while cap.isOpened():
 
     if prev_frame is not None and frame is not None:
         is_similar = are_images_similar(frame, prev_frame)
-    else:
-        is_similar = False
 
     if success and not is_similar:
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         hr, min, sec, msec = format_timestamp(timestamp)
-        formatted_timestamp = f"{hr}:{min}:{sec}:{msec}"
+        formatted_timestamp = f"{hr:02}:{min:02}:{sec:02},{msec}"
         timestamps.append(formatted_timestamp)
 
-        # read the text from frames
-        frame_data = reader.readtext(frame, detail=0)
-        text = ' '.join(frame_data)
-        translated_text = ''
-        if text != '':
-            translated_text = GoogleTranslator(source='ja', target='en').translate(text)
-
-        cv.putText(frame, translated_text, (20, 2000), font, 1, (255, 255, 255), 2, cv.LINE_AA)
-
-        # data_from_frames[frame_number] = [timestamp, frame_data]
-        # print(frame_data)
+        # read text from frames
+        frame_text = reader.readtext(frame, detail=0)
+        print(frame_text)
+        data_from_frames[frame_number] = {
+            'timestamp_start': formatted_timestamp,
+            'timestamp_end': formatted_timestamp,
+            'text': ''.join(frame_text),
+        }
+        if prev_frame is not None:
+            data_from_frames[frame_number - 1] = {
+                **data_from_frames[frame_number - 1],
+                'timestamp_end': formatted_timestamp
+            }
         # cv.imwrite(f'../frames/ {frame_number}.jpg', frame)
         cv.imshow('frame', frame)
         frame_number += 1
 
     prev_frame = frame
-    if cv.waitKey(1) == ord('q'):
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv.destroyAllWindows()
 
-#for key, data in data_from_frames.values():
-#   print(data)
-
-
+file = open('../subtitle/sub.srt', 'w')
+for number, value in data_from_frames.items():
+    file.write(str(number))
+    file.write('\n')
+    file.write(f"{value['timestamp_start']} --> {value['timestamp_end']} \n")
+    file.write(value['text'])
+    file.write('\n')
+    file.write('\n')
 
